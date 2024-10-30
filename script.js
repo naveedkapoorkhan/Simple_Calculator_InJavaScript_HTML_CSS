5bad38adf21fd118bedccc2ac055fedd2ceaa584
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2024-10-29 23:15:19 -0700

    calculator all file is added

---
+++ script.js
@@ -0,0 +1,30 @@
+ (
+    function(){
+        let screen=document.querySelector(".screen");
+        let buttons=document.querySelector(".btn");
+        let clear=document.querySelector(".btn-clear");
+        let equal=document.querySelector(".btn-equal");
+        
+        buttons.forEach(function(button){
+            button.addEventListener("click",function(e){
+                let value = e.target.dataset.data-num;
+                screen.value += value;
+            })
+            
+        });
+        equal.addEventListener("click",function(e){
+            if(screen.value ===""){
+                screen.value="";
+            }
+            else{
+                let answer=eval(screen.value);
+                    screen.value = answer;
+                }
+            }
+        )
+        clear.addEventListener("click",function(e){
+            screen.value="";
+        })
+
+    }
+ )();
